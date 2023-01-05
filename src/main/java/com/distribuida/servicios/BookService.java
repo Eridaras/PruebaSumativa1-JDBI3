e468031710408b09a878a8fc56832febe0c74323
Eridaras <erickgordillo@hotmail.es>
2023-01-05 18:20:11 -0500

    Actualizacion de Dependencias y creacion de Metodos

--- src/main/java/com/distribuida/servicios/BookService.java
+++ src/main/java/com/distribuida/servicios/BookService.java
@@ -7,4 +7,7 @@ import java.util.List;
 public interface BookService {
     List<Book> allBooks();
     Book findByBook(Integer id);
+    void createBook(Book book);
+    void deleteBook(Integer id);
+    void updateBook(Book book,Integer id);
 }
--- src/main/java/com/distribuida/servicios/BookServiceImpl.java
+++ src/main/java/com/distribuida/servicios/BookServiceImpl.java
@@ -36,4 +36,33 @@ public class BookServiceImpl implements BookService {
         return book;
     }
 
+    @Override
+    public void createBook(Book book) {
+        Jdbi jdbi = Jdbi.create(dataSource);
+        Handle con = jdbi.open();
+        con.createUpdate("insert into books (isbn, title, author, price ) values (?, ?, ?, ?)")
+                .bind(0, book.getIsbn())
+                .bind(1, book.getTitle())
+                .bind(2, book.getAuthor())
+                .bind(3, book.getPrice())
+                .execute();
+    }
+    @Override
+    public void deleteBook(Integer id) {
+        Jdbi jdbi = Jdbi.create(dataSource);
+        Handle con = jdbi.open();
+        int count = con.execute("DELETE FROM \"books\" WHERE id =  ?", id);
+    }
+    @Override
+    public void updateBook(Book book, Integer id) {
+        Jdbi jdbi = Jdbi.create(dataSource);
+        Handle con = jdbi.open();
+        int count = con.createUpdate("UPDATE books SET isbn = :isbn, title=:title, author=:author, price=:price WHERE id = :id").
+                bind("isbn",book.getIsbn()).
+                bind("title", book.getTitle()).
+                bind("author", book.getAuthor()).
+                bind("price", book.getPrice()).
+                bind("id",id)
+                .execute();
+    }
 }
